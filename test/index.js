450ee86926335fe68354574c3ba9c234fee9645e
EmaSuriano <emanuel.suriano@example.com>
2019-09-16 21:42:53 +0200

    test: pr checker

--- test/index.js
+++ test/index.js
@@ -2,3 +2,5 @@ import { configure } from 'enzyme';
 import Adapter from './ReactSixteenAdapter'; // FIX: when enzyme supports for context tag, replace this with enzyme-adapter-react-16
 
 configure({ adapter: new Adapter() });
+
+console.log('testing pr checker');
